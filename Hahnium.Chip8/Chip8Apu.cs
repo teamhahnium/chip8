3fefc918c652f837461b722e39ca86098201f2c5
Eric Carter <eric.carter@cryowatt.com>
2020-05-21 20:34:00 -0700

    Basic platform

---
+++ Hahnium.Chip8/Chip8Apu.cs
@@ -0,0 +1,9 @@
+﻿namespace Hahnium.Chip8
+{
+    internal class Chip8Apu
+    {
+        public Chip8Apu(System.Memory<byte> ram)
+        {
+        }
+    }
+}
---
+++ Hahnium.Chip8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Hahnium.Chip8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var romImage = LoadRom(args[0]);
+            var platform = new Chip8Platform(romImage);
+
+            while (true)
+            {
+                platform.Tick();
+            }
+        }
+
+        private static ReadOnlyMemory<byte> LoadRom(string path)
+        {
+            return new ReadOnlyMemory<byte>(File.ReadAllBytes(path));
+        }
+    }
+}
